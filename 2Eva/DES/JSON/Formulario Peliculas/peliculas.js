4e02225d773baf281b170260560e64b46c79b49d
Richi <richitarazona@gmail.com>
2019-01-16 13:46:36 +0100

    cambios

---
+++ 2Eva/DES/JSON/Formulario Peliculas/peliculas.js
@@ -0,0 +1,38 @@
+var datos = {"pelis":[
+	{
+			"ID":"MOVIE01",
+			"titulo":"Back to Future",
+			"estreno":"1985",
+			"poster":"marty.png"
+	},
+	{
+			"ID":"MOVIE02",
+			"titulo":"Batman",
+			"estreno":"1989",
+			"poster":"keaton.jpg"
+	},
+	{
+			"ID":"MOVIE03",
+			"titulo":"Terminator II",
+			"estreno":"1992",
+			"poster":"arnold.jpg"
+	},
+	{
+			"ID":"MOVIE04",
+			"titulo":"Matrix",
+			"estreno":"1999",
+			"poster":"matrix.jpg"
+	},
+	{
+			"ID":"MOVIE05",
+			"titulo":"Kill Bill",
+			"estreno":"2003",
+			"poster":"killbill.jpg"
+	},
+	{
+			"ID":"MOVIE06",
+			"titulo":"Up",
+			"estreno":"2009",
+			"poster":"up.jpg"
+	}
+]}
