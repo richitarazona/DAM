89451595781b42d38bdbea31efd59daf83250823
Richi <richitarazona@gmail.com>
2019-01-16 14:05:09 +0100

    cambios

---
+++ 2Eva/DES/Grupos/festival.js
@@ -0,0 +1,38 @@
+var festivales = {
+  Festival: "Navar-Pop",
+  Ciudad: "Ablitas",
+  Anno: 2018,
+  Grupos: [
+    {
+      nombre: "Love of lesbian",
+      compañia: "Warner Music",
+      discos: [
+        "Microscopic Movies",
+        "Is It Fiction?",
+        "Ungravity",
+        "Maniobras de escapismo",
+        "Cuentos chinos para niños del Japón",
+        "1999",
+        "La noche eterna. Los días no vividos",
+        "El poeta Halley"
+      ]
+    },
+    {
+      nombre: "Izal",
+      compañia: "Autoeditado",
+      discos: [
+        "Teletransporte",
+        "Magia y efectos especiales",
+        "Agujeros de gusano",
+        "Copacabana",
+        "VIVO",
+        "Autoterapia"
+      ]
+    },
+    {
+      nombre: "Miss Caffeina",
+      compañia: "Warner Music",
+      discos: ["Imposibilidad del fenómeno", "De polvo y flores", "Detroit"]
+    }
+  ]
+};
