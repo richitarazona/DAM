b9c6f68d5df691e6a8281b752c3b893940f63560
Richi <richitarazona@gmail.com>
2019-01-17 11:53:31 +0100

    ult

--- 2Eva/DES/ValidacionExamen/script.js
+++ 2Eva/DES/ValidacionExamen/script.js
@@ -1,13 +1,14 @@
-function accion(){
+function accion() {
+  //   var modelo = document.getElementById("moviles").value;
+  //   var pulgadas = document.getElementById("pulgadas").value;
+  //   var accesorios = "";
+  //   var tarifaSeleccionada = "";
+  alert(miform.tarifa.value);
+  //   var tarifa = document.getElementsByName("tarifa").value;
+  //   tarifaSeleccionada = tarifa.value;
 
-    var modelo = document.getElementById("moviles").value;
-    var pulgadas = document.getElementById("pulgadas").value;
-    var accesorios = "";
-    for (let i = 0; i < miform.tarifa.length; i++) {
-        if (miform.accesorios[i].selected){
-        accesorios += miform.accesorios[i];}   
-    }
-    //var tarifa = document.getElementById("tarifa").value;
-    //var operador = document.getElementById("operador").value;
-    
+  //var tarifa = document.getElementById("tarifa").value;
+  //var operador = document.getElementById("operador").value;
+
+  //   alert(tarifaSeleccionada);
 }
